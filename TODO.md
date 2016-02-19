6c1b9046d0e7dc2dfa7b7c92393cd50a0193acc8
Joshua Kidd <kidd.josh.343@gmail.com>
2016-02-18 18:13:58 -0700

    changes for ember

---
+++ TODO.md
@@ -0,0 +1,4 @@
+## TODO Items
+
+- Token authentication... probably using Guardian or something similar.
+- Chat Channels and authentication for projects/ideas...
--- web/controllers/idea_controller.ex
+++ web/controllers/idea_controller.ex
@@ -2,20 +2,45 @@ defmodule Ideatap.IdeaController do
   use Ideatap.Web, :controller
   alias Ideatap.{Repo, Idea}
 
+  plug :scrub_params, "idea" when action in [:create, :update]
+
   def index(conn, _params) do
-    render conn, "index.json", ideas: Repo.all(Idea)
+    render conn, "index.json", ideas: Repo.all Idea
   end
 
-  def show(conn, params) do
-    IO.inspect params
+  def show(conn, %{"id" => id}) do
+    render conn, "show.json", idea: Repo.get Idea, id
   end
 
-  def create(conn, params) do
-    IO.inspect params
+  def update(conn, %{"idea" => idea_params}) do
+    changeset = Idea.changeset %Idea{}, idea_params
+    case Repo.update changeset do
+      {:ok, changeset} ->
+        render conn, "show.json", idea: changeset
+      {:error, changeset} ->
+        # ERRORS!
+    end
   end
 
-  def delete(conn, id) do
-    IO.puts id
+  def create(conn, %{"idea" => idea_params}) do
+    changeset = Idea.changeset %Idea{}, idea_params
+    case Repo.insert changeset do
+      {:ok, changeset} ->
+        render conn, "show.json", idea: changeset
+      {:error, changeset} ->
+        # ERRORRS!
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    idea = Repo.get Idea, id
+    case Repo.delete idea do
+      {:ok, deleted} ->
+        IO.inspect deleted
+        render conn, "show.json", idea: deleted
+      {:error, changeset} ->
+        # ERRORS!
+    end
   end
 
 end
